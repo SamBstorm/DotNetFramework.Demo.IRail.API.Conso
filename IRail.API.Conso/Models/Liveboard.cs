c3c6f0bfe6b1999073c55d5d7f0c033b951ae760
Samuel Legrain <samuel.legrain@bstorm.be>
2020-08-10 08:57:24 +0200

    Only Get on IRail API Liveboard

---
+++ IRail.API.Conso/Models/Liveboard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRail.API.Conso.Models
+{
+
+    public class Liveboard
+    {
+        public Departures Departures { get; set; }
+        public string Station { get; set; }
+        public Stationinfo Stationinfo { get; set; }
+        public double Timestamp { get; set; }
+        public string Version { get; set; }
+    }
+
+    public class Departures
+    {
+        public List<object> Departure { get; set; }
+        public double Number { get; set; }
+    }
+
+    public class Stationinfo
+    {
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The (iRail) id of the station. The NMBS id can be deducted by removing the leading
+        /// 'BE.NMBS.00'
+        /// </summary>
+        public string StationinfoId { get; set; }
+
+        /// <summary>
+        /// The longitude of the station
+        /// </summary>
+        public double LocationX { get; set; }
+
+        /// <summary>
+        /// The latitude of the station
+        /// </summary>
+        public double LocationY { get; set; }
+
+        /// <summary>
+        /// The default name of this station
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The consistent name of this station
+        /// </summary>
+        public string Standardname { get; set; }
+    }
+}
+
---
+++ IRail.API.Conso/Program.cs
@@ -0,0 +1,48 @@
+﻿using IRail.API.Conso.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace IRail.API.Conso
+{
+    class Program
+    {
+        private const string BASE_API_URI = "https://api.irail.be";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLiveboardFrom("Gent-Sint-Pieters"));
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BASE_API_URI);
+
+            //Définir le Médiatype approprié dans le Header Accept
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json", 0.8));
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml", 0.9));
+
+            HttpResponseMessage response =  client.GetAsync(Program.GetLiveboardFrom("Gent-Sint-Pieters")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                //Format de la réponse sérializé en class déterminée par l'utilisateur ATTENTION : System.Net.Http.Formatting à ajouter au Nuget si nécessaire
+                Liveboard info = response.Content.ReadAsAsync<Liveboard>().Result;
+                Console.WriteLine($"{info.Departures.Number}");
+                string jsonInfo = JsonConvert.SerializeObject(info, Formatting.Indented);
+                Console.WriteLine(jsonInfo);
+                //Format de la réponse en string (XML/JSON/...)
+                string resume = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(resume);
+            }
+            Console.ReadLine();
+        }
+
+        private static string GetLiveboardFrom(string stationName,string time = null, string date = null, string format = "json", string lang="en")
+        {
+            time = time ?? DateTime.Now.Hour.ToString("##") + DateTime.Now.Minute.ToString("##");
+            date = date ?? DateTime.Now.ToString("ddMMyy");
+            return $"/liveboard/?station={stationName}&time={time}&date={date}&lang={lang}&format={format}";
+        }
+    }
+}
